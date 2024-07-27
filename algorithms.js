f5dec76ae18f6cfbc763c2a7875f0a01ccf35d29
Bartosz Fryska <bartosz.fryska.91@gmail.com>
2024-07-27 13:04:01 +0200

    Almost finished all of the algorithms

--- algorithms.js
+++ algorithms.js
@@ -2,33 +2,31 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function MakeAllRectanglesInAColorAndRender( R, G, B ) {
+    colorArray.fill([R, G, B]);
+    RenderRectangles();
+}
+
 async function EndingGreenFinish() {
     for( let i = 0; i < unsortedArray.length; i++ ) {
         colorArray[i] = [20, 255, 20];
         RenderRectangles();
-        await sleep(40);
+        await sleep(20);
         colorArray[i] = [255, 255, 255];
     }
-
-    RenderRectangles(); // Finish
+    RenderRectangles(); // Finish the 'checking' phase and wait
     await sleep(80);
 
-    for( let i = 0; i < unsortedArray.length; i++ ) { // All in green
-        colorArray[i] = [20, 255, 20];
-    }
-    RenderRectangles();
+    await MakeAllRectanglesInAColorAndRender(0, 102, 0); // All in darker green
     await sleep(200);
 
-    for( let i = 0; i < unsortedArray.length; i++ ) { // All in white again
-        colorArray[i] = [255, 255, 255];
-    }
-    RenderRectangles();
+    await MakeAllRectanglesInAColorAndRender(255, 255, 255) // All in white again
     await sleep(80);
 }
 
-async function MarkTwoRectanglesInRedAndWaitThenDemarkThem(indexOne, indexTwo) {
-    colorArray[indexOne] = [255, 0, 0];
-    colorArray[indexTwo] = [255, 0, 0];
+async function MarkTwoRectanglesInAColorWaitThenDemarkThem(indexOne, indexTwo, R, G, B) {
+    colorArray[indexOne] = [R, G, B];
+    colorArray[indexTwo] = [R, G, B];
 
     RenderRectangles();
     await sleep(40); // Adjust wait time for visibility
@@ -39,15 +37,12 @@ async function MarkTwoRectanglesInRedAndWaitThenDemarkThem(indexOne, indexTwo) {
     RenderRectangles(); // Re-render to reset the color
 }
 
-async function SwapTwoRectangles(indexOne, indexTwo) {
-    //[unsortedArray[indexOne], unsortedArray[indexTwo]] = [unsortedArray[indexTwo], unsortedArray[indexOne]];
-    let temporary;
-    temporary = unsortedArray[indexOne];
-    unsortedArray[indexOne] = unsortedArray[indexTwo];
-    unsortedArray[indexTwo] = temporary;
+async function SwapTwoRectanglesRenderAndSleep(indexOne, indexTwo) {
+    [unsortedArray[indexOne], unsortedArray[indexTwo]] = [unsortedArray[indexTwo], unsortedArray[indexOne]];
+    [colorArray[indexOne], colorArray[indexTwo]] = [colorArray[indexTwo], colorArray[indexOne]];
 
     RenderRectangles();
-    await sleep(40); // Adjust wait time for visibility
+    await sleep(40);
 }
 
 var MergeSort = {
@@ -55,7 +50,7 @@ var MergeSort = {
         let sortedArray = [];
 
         while (arrayOne.length && arrayTwo.length) {
-            await MarkTwoRectanglesInRedAndWaitThenDemarkThem(indexOne, indexTwo);
+            await MarkTwoRectanglesInAColorWaitThenDemarkThem(indexOne, indexTwo, 255, 0, 0);
 
             if (arrayOne[0] < arrayTwo[0]) {
                 sortedArray.push(arrayOne.shift());
@@ -95,26 +90,24 @@ var MergeSort = {
 };
 
 var BubbleSort = {
-    Sort : async function ( array ) {
+    Sort: async function(array) {
         var swapped;
         var n = array.length;
 
-        for (let i = 0; i < n - 1; i++){
+        for (let i = 0; i < n - 1; i++) {
             swapped = false;
             for (let j = 0; j < n - i - 1; j++) {
-                await MarkTwoRectanglesInRedAndWaitThenDemarkThem( j, j+1 );
-                await sleep(40);
+                await MarkTwoRectanglesInAColorWaitThenDemarkThem(j, j + 1, 255, 0, 0);
 
-                if ( array[j] > array[j + 1] ) {
-                    [array[j], array[j+1]] = [array[j+1], array[j]];
+                if (array[j] > array[j + 1]) {
+                    await SwapTwoRectanglesRenderAndSleep(j, j + 1);
                     swapped = true;
-
-                    await SwapTwoRectangles( j, j+1 );
-                    await sleep (40);
                 }
+
+                await sleep(40); // Adjust wait time for visibility
             }
 
-            if (swapped == false) break; // if the array is sorted break
+            if (!swapped) break; // if the array is sorted, break
         }
 
         await EndingGreenFinish();
@@ -122,39 +115,45 @@ var BubbleSort = {
 }
 
 var BogoSort = {
-    IsSorted : function (array){
-        for(var i = 1; i < arr.length; i++)
-            if (a[i] < a[i-1])
-                return false
-
-        return true
+    IsSorted: async function (array) {
+        for (var i = 1; i < array.length; i++) {
+            await MarkTwoRectanglesInAColorWaitThenDemarkThem(i, i - 1, 0, 0, 102);
+            if (array[i] < array[i - 1]) {
+                await MakeAllRectanglesInAColorAndRender(50, 0, 0);
+                await sleep(400);
+                await MakeAllRectanglesInAColorAndRender(255, 255, 255);
+                await sleep(400);
+                return false;
+            }
+        }
+        return true;
     },
 
-    Shuffle : function(array){
-        var n = array.length
-        var i, j = n
+    Shuffle: async function (array) {
+        var n = array.length;
 
-        for (i = 0; i < array.length(); i++){
-            var ind = Math.floor( Math.random() * n )
+        for (var i = 0; i < n; i++) {
+            var ind = Math.floor(Math.random() * n);
 
-            // flag those rectangles in color, slow down and then decolor them
+            await MarkTwoRectanglesInAColorWaitThenDemarkThem(i, ind, 255, 0, 0);
 
-            [ array[j-i-1], array[ind] ] = [ array[ind], array[j-i-1] ]
-
-            // swap those rectangles
+            [array[i], array[ind]] = [array[ind], array[i]];
+            await sleep(200);
         }
 
-        return array
+        return array;
     },
 
-    Sort : function (array) {
-        while ( !IsSorted(array) )
-            array = Shuffle (array)
+    Sort: async function (array) {
+        while (!(await BogoSort.IsSorted(array))) {
+            array = await BogoSort.Shuffle(array);
+        }
+        await MakeAllRectanglesInAColorAndRender(0, 255, 0); // Mark sorted array in green
     }
 }
 
 var CoctailSort = {
-    Sort : function(array) {
+    Sort : async function (array) {
         let swapped = true;
         let start = 0;
         let end = array.length;
@@ -163,90 +162,118 @@ var CoctailSort = {
             swapped = false;
   
             for (let i = start; i < end - 1; ++i) {
-                // change the colors of rectangles and dechange them
+                await MarkTwoRectanglesInAColorWaitThenDemarkThem(i, i + 1, 255, 0, 0);
 
                 if (array[i] > array[i + 1]) {
                     [ array[i], array[i+1] ] = [ array[i+1], array[i] ]
                     swapped = true;
 
-                    // swap rectangles
+                    RenderRectangles();
+                    await sleep (80);
                 }
             }
   
-            if (swapped == false)
+            if (swapped == false){
                 break;
+            }
 
             swapped = false;
   
             end = end - 1;
   
             for (let i = end - 1; i >= start; i--) {
-                // change the colors of rectangles and dechange them
+                await MarkTwoRectanglesInAColorWaitThenDemarkThem(i, i + 1, 255, 0, 0);
 
                 if (array[i] > array[i + 1]) {
                     [ array[i], array[i+1] ] = [ array[i+1], array[i] ]
                     swapped = true;
 
-                    // swap rectangles
+                    RenderRectangles();
+                    await sleep (80);
                 }
             }
-  
-            start++
+            
+            if (swapped == false){
+                break;
+            }
+
+            start++;
         }
+
+        await EndingGreenFinish();
     }
 }
 
 var InsertionSort = {
-    Sort : function (array) {
+    Sort : async function (array) {
         n = array.length
         let i, key, j;  
         for (i = 1; i < n; i++) {  
-            key = arr[i];  
+            key = array[i];  
             j = i - 1;  
-    
-            while (j >= 0 && arr[j] > key) {
-                // flag that rectangle j and deflag it, and swap
             
-                arr[j + 1] = arr[j];  
+            await MarkTwoRectanglesInAColorWaitThenDemarkThem(i, i, 0, 0, 255);
+            await sleep(300);
+
+            while (j >= 0 && array[j] > key) {
+                await MarkTwoRectanglesInAColorWaitThenDemarkThem(j, j, 255, 0, 0);
+
+                array[j + 1] = array[j]; 
+                RenderRectangles();
+                await sleep(80);
+
                 j = j - 1;  
             }  
 
-            // change rectangles
-            arr[j + 1] = key;  
+            array[j + 1] = key;  
+            await MarkTwoRectanglesInAColorWaitThenDemarkThem(i, i, 0, 0, 255);
+            RenderRectangles();
+            await sleep(300);
         } 
+
+        await EndingGreenFinish();
     }
 }
 
 var QuickSort = {
-
-    Partition : function(array, low, high) {
-        let pivot = arr[high];
+    Partition: async function(array, low, high) {
+        let pivot = array[high];
         let i = low - 1;
-        // flag pivot one in different color
-      
-        for (let j = low; j <= high - 1; j++) {
-            if (arr[j] < pivot) {
-                i++
-                // mark those rectangles in red and then demark them
 
-                [arr[i], arr[j]] = [arr[j], arr[i]]
-                
-                // swap rectangles
+        colorArray[high] = [218, 165, 32];
+
+        for (let j = low; j <= high - 1; j++) {
+            if (array[j] < pivot) {
+                i++;
+                await MarkTwoRectanglesInAColorWaitThenDemarkThem(i, j, 255, 0, 0);
+
+                [array[i], array[j]] = [array[j], array[i]];
+                RenderRectangles();
+                await sleep(80);
             }
         }
-      
-        [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]]
 
-        // deflag pivot
-        return i + 1
+        [array[i + 1], array[high]] = [array[high], array[i + 1]];
+        colorArray[high] = [255, 255, 255];
+        await sleep(80);
+
+        return i + 1;
     },
     
-    Sort : function(array, low, high) {
+    QuickSort: async function(array, low, high) {
         if (low < high) {
-            let pi = Partition(array, low, high);
-      
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
+            let pi = await QuickSort.Partition(array, low, high);
+
+            await QuickSort.QuickSort(array, low, pi - 1);
+            await QuickSort.QuickSort(array, pi + 1, high);
         }
+    },
+
+    Sort: async function(array) {
+        await QuickSort.QuickSort(array, 0, array.length - 1);
+
+        await EndingGreenFinish();
+
+        return array;
     }
-}
+};
